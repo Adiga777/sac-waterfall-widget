2d1af733958554d6da995cd198a35ba61800c5cd
Tatyana <tatyana_doughouz@yahoo.com>
2025-07-17 15:28:27 +0200

    Update waterfallChart.js

--- waterfallChart.js
+++ waterfallChart.js
@@ -7,7 +7,7 @@
     {
       "kind": "main",
       "tag": "com-custom-waterfallchart",
-      "url": "https://example.com/waterfallChart.js",
+      "url": "https://raw.githubusercontent.com/Adiga777/sac-waterfall-widget/refs/heads/main/waterfallChart.js",
       "integrity": "sha384-abc123def456ghi789jkl012mno345pqr678stu901vwx234yz567abc890defgh"
     }
   ],
