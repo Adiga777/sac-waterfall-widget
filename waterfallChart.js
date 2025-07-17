bf53b0efe5e109395f5ccee49ae9645efb5af54a
Tatyana <tatyana_doughouz@yahoo.com>
2025-07-17 15:25:43 +0200

    Create waterfallChart.js

---
+++ waterfallChart.js
@@ -0,0 +1,17 @@
+{
+  "id": "com.custom.waterfallchart",
+  "version": "1.0.0",
+  "name": "WaterfallChartWidget",
+  "newInstancePrefix": "WaterfallChart",
+  "webcomponents": [
+    {
+      "kind": "main",
+      "tag": "com-custom-waterfallchart",
+      "url": "https://example.com/waterfallChart.js",
+      "integrity": "sha384-abc123def456ghi789jkl012mno345pqr678stu901vwx234yz567abc890defgh"
+    }
+  ],
+  "properties": {},
+  "methods": {},
+  "events": {}
+}
